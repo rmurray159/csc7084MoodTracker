bdb84059187b0fe763d6bd8d1e927981f6ea319e
Rachel M <96990138+rmurray159@users.noreply.github.com>
2024-01-24 19:56:37 +0000

    Setting up addsnapshot

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path');
-//const dotenv = require('dotenv').config({ path: './config.env' });
-//const router = require('./routes/myroutes');
+const PORT = 3000;
 
-const app = express();
-
+var app = express();
+// set the current template engine
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '/public')));
+// access to images, css stylesheets 
+app.use(express.static(__dirname + '/public'));
+
+
+
 app.use(morgan('tiny'));
-app.use(express.urlencoded({ extended: true }));
 
-//app.use('/', router);
+app.get('/', (req, res) => {
+    res.status(200);
+    //res.sendFile(__dirname + '/html/welcome.html');
+    res.render('addsnapshot', { first_name: "Rachel", timeofday: "afternoon" });
+    });
 
-// app.listen(process.env.PORT, (err) => {
-//     if (err) return console.log(err);
+// route for 404 page not found - should always be at the end 
+app.get('*', (req, res) => {
+    res.status(404);
+    res.send('<h1>Page not found!</h1>');
+    });
 
-//     console.log(`Express listening on port ${process.env.PORT}`);
-// });
+app.listen(PORT, (err) => {
+    if (err)
+        return console.log(err);
+    console.log(`Express Web Server listening on http://localhost:${PORT}`);
+});
